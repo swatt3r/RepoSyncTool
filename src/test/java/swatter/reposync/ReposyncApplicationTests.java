84fb3ed1a6f93fbb86f88434eff8359296f96c1c
swatter <ser4an007@yandex.ru>
2024-07-02 13:59:45 +0400

    git init

---
+++ src/test/java/swatter/reposync/ReposyncApplicationTests.java
@@ -0,0 +1,13 @@
+package swatter.reposync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReposyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
